8e215b4e6d438978c5084e18ed0610601b69fe61
Michael P. Starkweather <mpstark@gmail.com>
2019-09-18 18:49:27 -0500

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Harmony;
+using HBS.Logging;
+
+namespace RNGFix
+{
+    public static class Main
+    {
+        internal static ILog HBSLog;
+
+        public static void Init(string modDir, string settings)
+        {
+            var harmony = HarmonyInstance.Create("io.github.mpstark.RNGFix");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            HBSLog = Logger.GetLogger("RNGFix");
+        }
+    }
+}
